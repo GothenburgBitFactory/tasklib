8ad57d84e608417867885cee2afe898e968ce3f3
Tomas Babej <tomasbabej@gmail.com>
2015-01-08 07:11:42 +0100

    Setup: Add pytz as dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     test_suite='tasklib.tests',
-    install_requires=['six==1.5.2'],
+    install_requires=['six==1.5.2', 'pytz'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
