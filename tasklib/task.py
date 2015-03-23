5ca9944bd0c33603dfc37d2e5f044cf03f77a2ea
Tomas Babej <tomasbabej@gmail.com>
2015-03-23 06:57:29 +0100

    Task: Add stop method

--- tasklib/task.py
+++ tasklib/task.py
@@ -416,6 +416,12 @@ class Task(TaskResource):
         """
         pass
 
+    class InactiveTask(Exception):
+        """
+        Raised when the operation cannot be performed on an inactive task.
+        """
+        pass
+
     class NotSaved(Exception):
         """
         Raised when the operation cannot be performed on the task, because
@@ -590,6 +596,21 @@ class Task(TaskResource):
             raise Task.DeletedTask("Deleted task cannot be started")
 
         self.warrior.execute_command([self['uuid'], 'start'])
+
+        # Refresh the status again, so that we have updated info stored
+        self.refresh(only_fields=['status', 'start'])
+
+    def stop(self):
+        if not self.saved:
+            raise Task.NotSaved("Task needs to be saved before it can be stopped")
+
+        # Refresh, and raise exception if task is already completed/deleted
+        self.refresh(only_fields=['status'])
+
+        if not self.active:
+            raise Task.InactiveTask("Cannot stop an inactive task")
+
+        self.warrior.execute_command([self['uuid'], 'stop'])
 
         # Refresh the status again, so that we have updated info stored
         self.refresh(only_fields=['status', 'start'])
