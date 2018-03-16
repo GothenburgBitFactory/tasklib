1094a6f6b25936381fe77b95ed154e01c9e0523a
Lyz <lyz@riseup.net>
2018-03-16 09:30:17 +0100

    * Fixed the parsing of undo.data line with more than one space between the attributes

--- tasklib/backends.py
+++ tasklib/backends.py
@@ -480,13 +480,14 @@ class TaskHistory(TaskWarrior):
         data_line = re.sub('" ', '", ', data_line)
 
         for key in self.backend.available_task_attrs:
-            data_line = re.sub(re.compile('({|, )(' + key + '):'),
+            data_line = re.sub(re.compile('({|, +)(' + key + '):'),
                                r'\1"\2":', data_line)
         data_line = re.sub(r'(annotation_\d*):', r'"\1":', data_line)
         try:
             history_entry = json.loads(data_line)
         except Exception:
             print('Error parsing line:\n{}'.format(data_line))
+
             raise
 
         for key in self.backend.available_task_attrs:
