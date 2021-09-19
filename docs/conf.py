8a3559b7f98a650016fb66293d2927554c90d910
Tomas Babej <tomas@tbabej.com>
2021-09-19 13:58:20 -0400

    docs: Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'tasklib'
-copyright = u'2014, Rob Golding'
+copyright = u'2014 - 2021, Rob Golding & Gothenburg Bit Factory'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
